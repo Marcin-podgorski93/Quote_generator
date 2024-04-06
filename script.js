3d567a383ee83668a4c27296a94628832f3970af
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-04-06 23:51:04 +0200

    add random quote

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+// Show new Quote
+function newQuote() {
+    // Pick a random quote
+    const quote = data[Math.floor(Math.random() * data.length)];
+    console.log(quote);
+}
 
+// Get quotes from API
 
 async function QuoteApi() {
     const ApiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     const resp = await fetch(ApiUrl);
-    const data = await resp.json();
-    console.log(data[2]);
+    data = await resp.json();
+    newQuote();
 }
 
 QuoteApi();
